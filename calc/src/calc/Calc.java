e2b858b3d978294eba3421c59c6f957c0f54cadc
zaharijs <52212903+zaharijs@users.noreply.github.com>
2019-06-26 23:30:33 +0300

    calc

---
+++ calc/src/calc/Calc.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calc;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Kārlis
+ */
+public class Calc {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in); 
+        
+        String txt = "x+y=";
+        
+        System.out.println("Please enter number x:");
+        int x = sc.nextInt();
+        
+        System.out.println("Please enter number y:");
+        int y = sc.nextInt();
+    
+        System.out.println(txt+(x+y));
+        
+                
+    }
+    
+}
