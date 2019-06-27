1ce09e8a19b6a9ba1b4e4620a575ff8ee1f1ca57
zaharijs <52212903+zaharijs@users.noreply.github.com>
2019-06-27 18:51:38 +0300

    calc_update

--- calc/src/calc/Calc.java
+++ calc/src/calc/Calc.java
@@ -18,39 +18,32 @@ public class Calc {
      */
     public static void main(String[] args) 
     {
-        //metodi, kurai padod ieksa 3 parametrus-
-        // 2 int tipa skaitli
-        //strings
-        //ievada skaitlus, plusu vai minusu un tad izpildam darbibu
-        {
         int x;
         int y;
         String calc;
-
 
         Scanner input = new Scanner(System.in);
 
         System.out.print("Please enter the first number: ");
         x = input.nextInt();
+        
+        Scanner cl = new Scanner(System.in);
+        System.out.print("Please enter + or - : ");
+        calc = cl.next();
 
         System.out.print("Please enter the second number: ");
         y = input.nextInt();
-
-        Scanner cl = new Scanner(System.in);
-
-        System.out.println("Please enter + or - : ");
-        calc = cl.next();
-
+               
         if (calc.equals("+"))
         {
             System.out.println("Your answer is: " + (x + y));
         }
-        if  (calc.equals("-"))
+        else if  (calc.equals("-"))
         {
             System.out.println("Your answer is: " + (x - y));
         }
-       }
-    }  }  
+    } 
+}   
         
         
         
