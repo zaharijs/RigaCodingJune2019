3e2d83c6f732d19ee606361d58adfdd25ad70392
zaharijs <52212903+zaharijs@users.noreply.github.com>
2019-07-01 20:51:56 +0300

    Day4 bubbles sorting

--- Day4/src/day4/ArrayTasks.java
+++ Day4/src/day4/ArrayTasks.java
@@ -34,5 +34,50 @@ public class ArrayTasks {
         System.out.println("Videjais ir " + (sum/numberArray.length));
         
     }
+    public void task2(){
+    int a = 5;
+    int b = 4;
     
+        System.out.println(a);
+        System.out.println(b);
+    int c = a; //c=5 , a=5, b=4
+    a = b;
+    b = c;
+    
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    
+        
+    }
+    public void masivs2(){
+    Scanner sc = new Scanner(System.in);
+    
+    System.out.println("Ievadiet masiva garumu: ");
+    
+    int size = sc.nextInt();
+    int[] numberArray = new int[size];
+    
+    double sum = 0;
+    
+    for(int i = 0; i<numberArray.length; i++){
+        System.out.println("Ievadiet masiva" + (i+1) + ". vertibu");
+         numberArray[i] = sc.nextInt();
+          
+    }
+    int n = numberArray.length;
+        for (int i = 0; i < n-1; i++)
+            for (int j = 0; j < n-i-1; j++)
+                if (numberArray[j] > numberArray[j+1])
+                {
+                    // swap temp and arr[i]
+                    int temp = numberArray[j];
+                    numberArray[j] = numberArray[j+1];
+                    numberArray[j+1] = temp;}
+                    for (int i=0; i<n; ++i)
+            System.out.print(numberArray[i]);
+        System.out.println();
+    }
+    
+   
     }
--- Day4/src/day4/Day4.java
+++ Day4/src/day4/Day4.java
@@ -17,7 +17,7 @@ public class Day4 {
     public static void main(String[] args) {
         // TODO code application logic here
         ArrayTasks ex = new ArrayTasks();
-        ex.masivs();
+        ex.masivs2();
         
     }
     
