7a7339f70978c0b0606b8ddaf338a95ebaa9c742
zaharijs <52212903+zaharijs@users.noreply.github.com>
2019-07-01 17:16:46 +0300

    Day3Done

--- Day3/src/day3/Day3.java
+++ Day3/src/day3/Day3.java
@@ -15,14 +15,32 @@ public class Day3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-//     NewClassExample example = new NewClassExample();
-//     example.sample1();
-//     NewClassExample.sample2();
-     Task tsk = new Task();
-     tsk.displayDay();
-     
-     tsk.factorial();
-     tsk.guessTheNumber();
+        // TODO code application logic here
+//        NewClassExample example = new NewClassExample();
+//        example.sample1();
+        
+//        NewClassExample.sample2();
+        //1.Uztaisam jaunu klasi- Tasks
+        //2.Klase metode, kas paprasa cilvekam ievadit skaitli
+        //3.Izvada attiecigo dienu, vai ari pasaka, ka ievade nav pareiza
+        //4.* try..catch parbaudam vai ievadits tiek cipars
+        Task tsk = new Task();
+//        tsk.displayDay();
+        
+        //1. Lietojam to pasu Tasks klasi
+        //2. Cilveks ievada veselu skaitli
+        //3. Mes sim skaitlim ar for() ciklu aprekinam faktoriali
+        //4. cilveks ievada 5- 5*4*3*2*1
+        //5. * try..catch
+        
+        tsk.factorial();
+        
+        //While
+        //1. Uztaisam ta, lai arpus cikla 1. cilveks ievada skaitli
+        //2. sakas while cikls. 2. cilveks censas uzminet skaitli
+        //3. Programma darbojas tik ilgi, kamer skaitlis nav uzminets
+        
+        tsk.guessTheNumber();
      
     }
     
